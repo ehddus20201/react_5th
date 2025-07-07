584137e362bdad01390f1ce00741ea230a0144ba
ehddus20201 <ehddus20201@naver.com>
2025-07-07 10:44:26 +0900

    컴포넌트 구현

--- 02-component-with-props/src/main.js
+++ 02-component-with-props/src/main.js
@@ -1,5 +1,5 @@
-import { createElement } from "./lib/react.js";
-import { createRoot } from "./lib/react-dom.js";
+import React, { createElement as h } from "./lib/react.js";
+import ReactDom, { createRoot } from "./lib/react-dom.js";
 
 const listData = {
   items: [
@@ -11,8 +11,82 @@ const listData = {
 };
 
 
+{/* <li class="item">
+        <img src="/planet/image-01.jpg" alt="" />
+        <span class="content">Life on Earth</span>
+        <button type="button" title="아이템 이동 (위/아래 화살표 키 활용)">
+          <img
+            src="./src/icons/icons.svg"
+            alt="아이템 이동 (위/아래 화살표 키 활용)"
+          />
+        </button>
+      </li> */}
+/* class syntax */
+
+// 1. PlanetItem
+
+class PlanetItem extends React.Component{
+
+  // constructor(props){
+  //   super();
+  //   console.log(props);
+    
+  // }
+  render(){
+
+    const {title, id} = this.props;
+
+    return h('li',
+      {className:'item',id:1},
+      h('img',{src:`/planet/image-0${id}.jpg`, alt:''}),
+      h('span',{className:'content'},title),
+      h('button',{type:"button",title:'move item'},
+        h('img',{src:"./src/icons/icons.svg",alt:'move item'})
+      ),
+    )
+  }
+}
+
+class PlanetList extends React.Component{
+
+  
+  render() {
+    const {lang, children} = this.props;
+
+    return h("ul", { className: "planet", lang: "en" },
+      children
+    )
+      
+  }
 
 
+}
+
+
+
+// 2. PlanetList
+
+// 3. PlanetPage
+class PlanetPage extends React.Component{
+  render(){
+    return h(
+      PlanetList,{
+        lang:'en',
+        children: listData.items.map(({id,title}) => h(PlanetItem, { key:id, id, title }))
+      }
+    )
+  }
+}
+
+
+/* function syntax */
+
+
+
+const container = document.getElementById('app')
+const reactrDomRoot = ReactDom.createRoot(container);
+
+reactrDomRoot.render(h(PlanetPage));
 
 
 
